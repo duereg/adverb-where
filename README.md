f1aaf5dd2b17f7dc05c86a2be303b7070101c4b9
Matt <duereg@hotmail.com>
2014-05-16 23:29:16 -0700

    initial commit.

---
+++ README.md
@@ -0,0 +1,23 @@
+[![Build Status](https://travis-ci.org/duereg/adverb-where.svg?branch=master)](https://travis-ci.org/duereg/adverb-where)
+
+# Word Complexity
+
+npm module for checking for adverbs
+
+## Install
+
+```shell
+npm install adverb-where
+```
+
+## Use
+
+```javascript
+var complexity = require('adverb-where');
+
+var problems = complexity('Allegedly, this sentence is terrible.');
+// problems -> [{ match: "Allegedly", index: 0, offset: 9 }]
+```
+
+## License
+MIT
---
+++ test/basic.spec.js
@@ -0,0 +1,22 @@
+var adverbs = require('../adverbs');
+var badWordInSentence = 'Allegedly, this sentence is terrible.';
+var goodSentence = 'The good dog jumps over the bad cat.';
+
+describe('word-complexity', function () {
+
+  describe('a sentence filled with adverbs', function() {
+    var results = null;
+
+    beforeEach(function() {
+      results = adverbs(badWordInSentence);
+    });
+
+    it('will not escape notice', function () {
+      expect(results[0].match).toEqual('Allegedly');
+    });
+  })
+
+  it('should not have a problem with a short sentence', function () {
+    expect(adverbs(goodSentence)).toEqual([]);
+  });
+});
